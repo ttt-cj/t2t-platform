081c2e24eb15dad69ddbb43d332bff013430d7b4
CJ <81480405+ttt-cj@users.noreply.github.com>
2023-09-30 11:07:59 +0530

    test

--- js/projects-4fetch.js
+++ js/projects-4fetch.js
@@ -1,6 +1,10 @@
 const projectList = document.querySelector('.project-list');
 
-fetch(API_URL + '?action=get-projects');
+fetch(API_URL + '?action=get-4projects')
+.then(response => response.json())
+  .then(data => {
+
+  })
 
 
 console.log('hooo hooo');
