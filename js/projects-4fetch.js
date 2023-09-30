e8e5e6bb828f206b08f0b9830632daf55791bd8a
CJ <81480405+ttt-cj@users.noreply.github.com>
2023-09-30 13:38:22 +0530

    d

--- js/projects-4fetch.js
+++ js/projects-4fetch.js
@@ -16,31 +16,6 @@ fetch(API_URL + '?action=get-projects')
       title.textContent = project.Title;
       listItem.appendChild(title);
 
-      // Create project details
-      const details = document.createElement('div');
-      details.classList.add('project-details');
-
-      // Create project detail items
-      const detailItems = [
-        { icon: 'fas fa-building', label: 'Company', value: project.Company },
-        { icon: 'fas fa-bullseye', label: 'Goal', value: project.Goal },
-        { icon: 'fas fa-users', label: 'Minimum Team', value: project.Minimum_Team },
-        { icon: 'far fa-clock', label: 'Duration', value: project.Duration }
-      ];
-
-      // Loop through each detail item and create a span element
-      detailItems.forEach(item => {
-        const div = document.createElement('div');
-        const span = document.createElement('span');
-        span.innerHTML = `<i class="${item.icon}"></i>&nbsp; ${item.label}:`;
-        div.appendChild(span);
-
-        const value = document.createElement('p');
-        value.innerHTML = item.value;
-        div.appendChild(value);
-        details.appendChild(div);
-      });
-
       listItem.appendChild(details);
 
       // Create bid button
@@ -54,4 +29,4 @@ fetch(API_URL + '?action=get-projects')
     });
   })
   .catch(error => console.error(error));
-  console.log('test2');
+  console.log('test1');
